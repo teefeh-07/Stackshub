d6064ce5fdfae325e266a00ee5f4504b1a53315c
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-21 10:56:57 +0100

    feat: add line 5 for ChainHooksClientIntegration

--- frontend/src/services/ChainHooksClientIntegration.tsx
+++ frontend/src/services/ChainHooksClientIntegration.tsx
@@ -2,3 +2,4 @@ import { ChainhookClient } from '@hirosystems/chainhooks-client';
 export const client = new ChainhookClient({
   baseUrl: 'https://api.mainnet.hiro.so',
   apiKey: process.env.HIRO_API_KEY || ''
+});
