f7a88bafdf640c7066983da9bf2dd360166f6744
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-21 10:55:12 +0100

    feat: add line 13 for WalletConnectUI

--- frontend/src/components/WalletConnectUI.tsx
+++ frontend/src/components/WalletConnectUI.tsx
@@ -10,3 +10,4 @@ export default function WalletConnectUI() {
   };
   return (
     <div className='wc-container'>
+      <button onClick={onConnect}>{connected ? 'Connected' : 'Connect WalletConnect'}</button>
