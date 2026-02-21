7a55c925f8dbe6f06916511bf7cc002d0d242ff9
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-21 10:54:09 +0100

    feat: add line 6 for WalletConnectUI

--- frontend/src/components/WalletConnectUI.tsx
+++ frontend/src/components/WalletConnectUI.tsx
@@ -3,3 +3,4 @@ import { WalletConnectModal } from '@walletconnect/modal';
 const projectId = 'your_project_id_here';
 const modal = new WalletConnectModal({ projectId });
 export default function WalletConnectUI() {
+  const [connected, setConnected] = useState(false);
