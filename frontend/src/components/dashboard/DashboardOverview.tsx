f0347d1d20affd0f3875599d06fcb9079499b7ca
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-21 11:18:12 +0100

    feat: add line 4 functionality for DashboardOverview

--- frontend/src/components/dashboard/DashboardOverview.tsx
+++ frontend/src/components/dashboard/DashboardOverview.tsx
@@ -1,3 +1,4 @@
 import React, { useState, useEffect } from 'react';
 import { useStacks } from '../../providers/StacksProvider';
 import { formatBalance } from '../../utils/formatters';
+export default function DashboardOverview() {
