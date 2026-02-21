69b753375a9fb015172f200ea001b43688d92ce4
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-21 10:51:23 +0100

    feat: add line 5 for StacksProvider

--- frontend/src/providers/StacksProvider.tsx
+++ frontend/src/providers/StacksProvider.tsx
@@ -2,3 +2,4 @@ import React, { createContext, useContext, useState } from 'react';
 import { AppConfig, UserSession, showConnect } from '@stacks/connect';
 import { StacksMainnet, StacksTestnet } from '@stacks/network';
 export const appConfig = new AppConfig(['store_write', 'publish_data']);
+export const userSession = new UserSession({ appConfig });
